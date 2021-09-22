96db9e034bb7fa623bf6d626ce6a42f0843348cf
zs17 <61650021+zs17@users.noreply.github.com>
2021-09-22 11:41:15 +0800

    Add files via upload

---
+++ UserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL
+{
+    class UserInfo
+    {
+        private UserInfo() { }
+        private static UserInfo _instance = new UserInfo();
+        public static UserInfo Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        string cns = AppConfigurtaionServices.Configuration.GetConnectionString("cns");
+        public string UserCheck(string UserName)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "select password from userinfo where username=@username;";
+                return cn.ExecuteScalar<string>(sql, new { username = UserName });
+            }
+        }
+    }
+}
