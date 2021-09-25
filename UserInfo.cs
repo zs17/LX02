3869cacde7f944871eefe91b4e5ee5e863098aa8
zs17 <61650021+zs17@users.noreply.github.com>
2021-09-25 21:33:16 +0800

    Add files via upload

--- UserInfo.cs
+++ UserInfo.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace DAL
 {
-    class UserInfo
+    public class UserInfo
     {
         private UserInfo() { }
         private static UserInfo _instance = new UserInfo();
@@ -22,11 +22,69 @@ namespace DAL
         string cns = AppConfigurtaionServices.Configuration.GetConnectionString("cns");
         public string UserCheck(string UserName)
         {
-            using (IDbConnection cn = new MySqlConnection(cns))
+            using(IDbConnection cn=new MySqlConnection(cns))
             {
                 string sql = "select password from userinfo where username=@username;";
                 return cn.ExecuteScalar<string>(sql, new { username = UserName });
             }
         }
+        public Model.UserInfo GetModel(string UserName)
+        {
+            using(IDbConnection cn=new MySqlConnection(cns))
+            {
+                string sql = "select * from userinfo where username=@username";
+                return cn.QueryFirstOrDefault<Model.UserInfo>(sql, new { username = UserName });
+            }
+        }
+        public IEnumerable<Model.UserInfo> GetAll()
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "select * from userinfo";
+                return cn.Query<Model.UserInfo>(sql);
+            }
+        }
+        public int GetCount()
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "select count(1) from userinfo";
+                return cn.ExecuteScalar<int>(sql);
+            }
+        }
+        public IEnumerable<Model.UserInfoNo> GetPage(Model.Page page)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "with a as(select row_number() over(order by username) as num,userinfo.* from userinfo)";
+                sql += "select * from a where num between (@pageIndex-1)*@pageSize+1 and @pageIndex*@pageSize";
+                return cn.Query<Model.UserInfoNo>(sql, page);
+            }
+        }
+        public int Add(Model.UserInfo user)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "insert into userinfo values(@userName,@passWord,@qq,@email,@type,@userImg);";
+                return cn.Execute(sql, user);
+            }
+        }
+        public int Update(Model.UserInfo user)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "update userinfo set password=@passWord,qq=@qq,email=@email,type=@type,userimg=@userImg where username=@userName;";
+                return cn.Execute(sql, user);
+            }
+        }
+        public int Delete(string username)
+        {
+            using (IDbConnection cn = new MySqlConnection(cns))
+            {
+                string sql = "delete from userinfo where username=@userName";
+                return cn.Execute(sql, new { username = username });
+            }
+        }
     }
+
 }
