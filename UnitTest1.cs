5db4931cf25cf61a504dc39cafe30c976286d860
zs17 <61650021+zs17@users.noreply.github.com>
2021-09-25 21:32:57 +0800

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -11,7 +11,22 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod1()
         {
-                Console.WriteLine(DAL.UserInfo.Instance.UserCheck("Admin"));
+            Console.WriteLine(DAL.UserInfo.Instance.UserCheck("Admin"));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Model.UserInfo user = new Model.UserInfo { userName = "Guest", passWord = "sql.123", type = "π‹¿Ì‘±" };
+            Console.WriteLine(DAL.UserInfo.Instance.Add(user));
+            Console.WriteLine(JsonConvert.SerializeObject(DAL.UserInfo.Instance.GetAll()));
+            user.qq = "104512708";
+            Console.WriteLine(DAL.UserInfo.Instance.Update(user));
+            var model = DAL.UserInfo.Instance.GetModel("Guest");
+            Console.WriteLine(JsonConvert.SerializeObject(model));
+            Console.WriteLine(DAL.UserInfo.Instance.Delete("Guest"));
+            var page = DAL.UserInfo.Instance.GetPage(new Model.Page { pageIndex = 2, pageSize = 2 });
+            Console.WriteLine(JsonConvert.SerializeObject(page));
+            Console.WriteLine(DAL.UserInfo.Instance.GetCount());
         }
     }
 }
